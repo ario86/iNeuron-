71a828e0f7554b516f59c94719c351adb8d68cf1
Aritrajit <aritrajit2000sarkar@gmail.com>
2022-07-12 00:15:33 +0530

    Delete main1.py

--- main1.py
+++
@@ -1,8 +0,0 @@
-x = input('enter 1st num: ')
-y = input('enter 2nd num: ')
-
-
-sum = float(x) + float(y)
-
-print('The sum of {0} and {1} is {2}'.format(x, y, sum))
-print("The Division of {0} and {1} = {2}".format(x, y, div))
